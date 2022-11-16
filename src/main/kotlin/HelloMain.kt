4eca684f19fa3ff36119457d0995d386c80b7f4c
Zijian Peng <282308044@qq.com>
2022-11-16 22:50:22 +0800

    add Clock producer

--- src/main/kotlin/HelloMain.kt
+++ src/main/kotlin/HelloMain.kt
@@ -4,6 +4,8 @@ import io.quarkus.runtime.QuarkusApplication
 import io.quarkus.runtime.annotations.QuarkusMain
 import mu.KLogger
 import mu.KotlinLogging
+import java.time.Clock
+import java.time.Instant
 import javax.inject.Inject
 
 @QuarkusMain
@@ -14,9 +16,13 @@ class HelloMain(
     @Inject
     lateinit var putObjectService: PutObjectService
 
+    @Inject
+    lateinit var clock: Clock
+
     @Throws(Exception::class)
     override fun run(vararg args: String): Int {
-        println("Hello you")
+        val nowIs = Instant.now(clock)
+        println("Hello you - $nowIs")
         logger.info { "logging ${putObjectService.javaClass}" }
         return 0
     }
---
+++ src/main/kotlin/config/ClockProducer.kt
@@ -0,0 +1,15 @@
+package com.zijian
+
+import java.time.Clock
+import java.time.Instant.EPOCH
+import java.time.ZoneId
+import javax.enterprise.context.ApplicationScoped
+import javax.enterprise.inject.Produces
+
+@ApplicationScoped
+class ClockProducer {
+
+    @get:Produces
+    val clock: Clock
+        get() = Clock.fixed(EPOCH, ZoneId.of("UTC"))
+}
